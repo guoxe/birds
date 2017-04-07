305bb2014ad91c081f6db18308b89fddd318afdb
go <gustaf@oxenstierna.org>
2017-04-07 11:36:54 +0200

    Finished the tool for generating training data

--- generate_training.m
+++ generate_training.m
@@ -1,6 +1,36 @@
+clc;
+clear;
 imagefiles = dir('videos/frames/*.jpg');
+fig = figure();
+data_idx = 1;
 for i=1:length(imagefiles)
     currentfile = imagefiles(i).name;
     imagesc(imread(strcat('videos/frames/',currentfile)));
-    [x,y] = ginput(4);
+    keydown = waitforbuttonpress();
+    if (keydown ~= 0)%skip the image
+        k = get(fig, 'CurrentCharacter');
+        if (strcmp(k,'ESC') == 0)
+            break;
+        end
+        continue;
+    else
+       %start by capturing positive examples
+        title('Click on the birds for positive examples');
+        [x,y] = ginput(4);
+        hold on;
+        plot(x,y,'*');
+        hold off;
+        s = struct('positive', [x y], 'imfile', currentfile);
+        title('click outside the birds for negative examples');
+        [x,y] = ginput(12); %gather negative examples for images
+        hold on;
+        plot(x,y,'or');
+        hold off;
+        s.negative = [x y];
+        pause(0.5);
+        training_data(data_idx) = s;
+        data_idx = data_idx + 1;
+    end
 end
+save('training_data.mat', 'training_data');
+close(fig);
