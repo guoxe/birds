63b6e73d8fb99636a4f9f0380ef595b9665e99c2
go <gustaf@oxenstierna.org>
2017-04-25 10:47:01 +0200

    added a quick script to check training data, experimented with logistic regression

--- test.m
+++ test.m
@@ -13,9 +13,66 @@ D=abs(O-O1);
 D(~M)=0;
 D(D>o*.99)=o-D(D>o*.99);
 mean2(abs(D));
-%H1=gradientHist(M1,O1,2,6,0);
-%figure(1);
-%montage2(H1);
+H1=gradientHist(M1,O1,1,6,0);
+figure(1);
+montage2(H1);
 %H2=gradientHist(M1,O1,2,6,1);
 %figure(2);
 %montage2(H2);
+
+%% begin experimentation with loading image data
+clear;
+clc;
+load training_data_2017_04_18.mat;
+im = imread(strcat('videos/frames/',training_data(1).imfile));
+I=im(:,:,1);%same value in all 3 channels so we dont have to convert
+x = round(training_data(1).positive(1:4));
+y = round(training_data(1).positive(5:8));
+%cut out a 20x20 frame around a positive example
+b=imcrop(I,'single', [x(1)-20 y(1)-20 39 39]);
+%plot the image with first positive example coordinates
+imshow(I);
+hold on;
+plot([x(1)-20 x(1)-20 x(1)+20 x(1) + 20], [y(1) - 20 y(1) + 20 y(1) - 20 y(1) + 20], '*r');
+hold off;
+figure();
+imshow(b);
+
+%%
+clear;
+clc;
+load training_data_2017_04_18.mat;
+for i=1:size(training_data,2)
+    imshow(strcat('videos/frames/',training_data(i).imfile));
+    fprintf('element number: %d\n', i);
+    hold on;
+    pos_x = training_data(i).positive(:,1);
+    pos_y = training_data(i).positive(:,2);
+    neg_x = training_data(i).negative(:,1);
+    neg_y = training_data(i).negative(:,2);
+    plot(pos_x, pos_y, '*r');
+    plot(neg_x, neg_y, 'ob');
+    hold off;
+    keydown=waitforbuttonpress();
+end
+
+%% test glmfit
+clear;
+clc;
+load training_data_clean.mat;
+im = imread(strcat('videos/frames/',training_data(1).imfile));
+I=im(:,:,1);%same value in all 3 channels so we dont have to convert
+x = round(training_data(1).positive(1:4));
+y = round(training_data(1).positive(5:8));
+%cut out a 20x20 frame around a positive example
+b=imcrop(I,'single', [x(1)-20 y(1)-20 39 39]);
+b = reshape(b, [1 1600]);
+b = im2double(b);
+b = (b-mean(b)) ./ std(b);
+c = imcrop(I, 'single', [x(2) - 20 y(2) - 20 39 39]);
+c = reshape(c, [1 1600]);
+c = im2double(c);
+c = (c-mean(c)) ./ std(c);
+X = [b c];
+Y = ones([1 2]);
+B = glmfit(X, Y, 'binomial', 'link', 'logit');
